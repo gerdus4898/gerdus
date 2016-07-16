4dca5c1e2d39cfbb0869814de44fe5dfbe0a5249
gerdus4898 <mcmodder007@gmail.com>
2016-07-16 08:20:19 +0700

    Update gerdusBoss.js

--- gerdusBoss.js
+++ gerdusBoss.js
@@ -1,1 +1,19 @@
+//Mod by gerdus4898 
 
+//Define all we need
+var ticktoggler = false;
+var tick;
+var boss;
+var bossExist = false;
+
+//When someone join a level
+function newLevel(){
+  clientMessage("Thank you for downloading gerdusBoss mod by gerdus4898");
+}
+//boss spawning system
+function useItem(x, y, z, itemId, blockId){
+  if(itemId== && blockId==57)
+}
+
+
+//minion spawning system
