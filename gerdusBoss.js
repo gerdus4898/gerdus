dd67d2963c83f798f27de3e8c37400191e415580
gerdus4898 <mcmodder007@gmail.com>
2016-07-16 08:26:44 +0700

    Update gerdusBoss.js

--- gerdusBoss.js
+++ gerdusBoss.js
@@ -6,10 +6,16 @@ var tick;
 var boss;
 var bossExist = false;
 
+//Custom Stick
+ModPE.setItem(2500, "stick", 0, "Gerdus's Lucky Stick")
+
+
 //When someone join a level
 function newLevel(){
   clientMessage("Thank you for downloading gerdusBoss mod by gerdus4898");
+  clientMessage("Checkout more of his creation on https://github.com/gerdus4898")
 }
+
 //boss spawning system
 function useItem(x, y, z, itemId, blockId){
   if(itemId== && blockId==57)
